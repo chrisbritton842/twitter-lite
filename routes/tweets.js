b260a7e15fb960e199bde5f842fd992d2f3e3a3f
Stan <stantm@gmail.com>
2022-02-09 14:51:53 -0800

    phase 3 upto GET /tweets/:id

--- routes/tweets.js
+++ routes/tweets.js
@@ -8,11 +8,32 @@ const { Tweet } = db;
 const asyncHandler = (handler) => (req, res, next) =>
     handler(req, res, next).catch(next);
 
+const tweetNotFoundError = (id) => {
+    const err = Error(`Tweet with id of ${id} could not be found.`);
+    err.title = "Tweet not found.";
+    err.status = 404;
+    return err;
+}
+
 router.get(
     "/", 
     asyncHandler(async (req, res) => {
         const tweets = await Tweet.findAll();
         res.json({ tweets });
+    })
+);
+
+router.get(
+    "/:id(\\d+)",
+    asyncHandler(async (req, res, next) => {
+        const tweetId = parseInt(req.params.id, 10);
+        const tweet = await Tweet.findByPk(tweetId);
+        
+        if (tweet) {
+            res.json({ tweet });
+        } else {
+            next(tweetNotFoundError(tweetId))
+        }
     })
 );
 
